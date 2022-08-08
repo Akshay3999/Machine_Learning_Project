06ac947732bc5158c252bb657e8f81b1f70d476b
Akshay tate <tateakshay399@gmail.com>
2022-08-08 13:07:20 +0530

    update

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+# Declaring variables for setup functions
+PROJECT_NAME="InsurancePremiumPrediction"
+VERSION="0.0.1"
+AUTHOR="Akshay Tate"
+DESCRIPTION="This is my first self made project"
+PACKAGES=["Insurance"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list()->List[str]:
+
+    """
+    Description: This function is going to return list of requirement
+    mention in requirementd.txt file
+    
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    authOr=AUTHOR,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
+
+)
