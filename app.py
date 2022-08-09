f45d2727e205df3681c7f15b55828d8a9510ba4b
Akshay tate <tateakshay399@gmail.com>
2022-08-09 11:44:47 +0530

    logger and exception completed

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
-
+import sys
+from InsurancePremiumPrediction.logger import logging
+from InsurancePremiumPrediction.exception import InsuranceException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+
+    except Exception as e:
+        Insurance = InsuranceException(e,sys)
+        logging.info(Insurance.error_message)
+        logging.info("We are testing logging module")
     return "CI CD pipline has been established and rerun"
 
 if __name__=="__main__":
